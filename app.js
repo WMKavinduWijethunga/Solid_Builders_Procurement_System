474dd6c45bff93dec216348390d3212e6f362ab1
WMKavinduWijethunga <it19981154@my.sliit.lk>
2021-10-02 18:14:43 +0530

    Quotation insert changes committed.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,36 +10,51 @@ const port = process.env.PORT || 5000;
 //parssing midlwares
 //parse application/x-www-form-urlencoded
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Templating Engine
-app.engine('hbs',exphbs({extname: '.hbs'}));
-app.set('view engine', 'hbs'); 
+app.engine("hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 //connection pool
 const pool = mysql.createPool({
-    connectionLimit : 100,
-    host            : process.env.DB_HOST,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME,
-    port            : 3307
-
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 3307,
 });
 
 //connect to DB
-pool.getConnection((err, connection) =>{
-    if(err) throw err; // not connected
-    console.log('Connected as ID' + connection.threadId);
+pool.getConnection((err, connection) => {
+  if (err) throw err; // not connected
+  console.log("Connected as ID" + connection.threadId);
 });
-app.use('/',require('./server/routes/main'));
-app.use('/',require('./server/routes/supplier'));
-app.use('/',require('./server/routes/staff'));
-app.use('/',require('./server/routes/management'));
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/supplier"));
+app.use("/", require("./server/routes/staff"));
+app.use("/", require("./server/routes/management"));
 
+let number = 0;
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+      getShortComment() {
+        number = number + 1;
+        return number;
+      },
+      clearNumber() {
+        number = 0;
+      },
+    },
+  })
+);
 
-app.listen(port,() => console.log(`Listening on port ${port}`));
-
+app.listen(port, () => console.log(`Listening on port ${port}`));

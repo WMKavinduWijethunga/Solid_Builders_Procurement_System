23155ded4d5596c6f674a7eed954446754680aa2
WMKavinduWijethunga <it19981154@my.sliit.lk>
2021-10-05 08:31:52 +0530

    supplier approved quotation edit committed.

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -13,96 +13,99 @@ const pool = mysql.createPool({
 });
 
 exports.viewPurchasedOrder = (req, res) => {
-    
 
+    let supID = req.params.supID;
     //connect to DB
     pool.getConnection((err, connection) => {
         if (err) throw err; // not connected
         console.log('Connected as ID' + connection.threadId);
 
-        connection.query('SELECT * FROM solidbuilders.purchaseorder where approval =? ',["Approve"], (err,rows) =>{
-            
+        connection.query('SELECT * FROM solidbuilders.purchaseorder where approval =? ', ["Approve"], (err, rows) => {
+
             connection.release();
 
-            if(!err){ 
-                res.render('supplierViewPurchasedOrder', {rows});
-            }else{
+            if (!err) {
+
+                rows[0].supID = supID;
+
+                res.render('supplierViewPurchasedOrder', { rows, supID: supID });
+            } else {
                 console.log(err);
             }
 
-            
+
         });
-       
+
     });
 
 }
 
 //view for mangement
 exports.viewPOManagement = (req, res) => {
-    
+
 
     //connect to DB
     pool.getConnection((err, connection) => {
         if (err) throw err; // not connected
         console.log('Connected as ID' + connection.threadId);
 
-        connection.query('SELECT * FROM solidbuilders.purchaseorder WHERE approval = ? ',["Management"], (err,rows) =>{
-            
+        connection.query('SELECT * FROM solidbuilders.purchaseorder WHERE approval = ? ', ["Management"], (err, rows) => {
+
             connection.release();
 
-            if(!err){ 
-                res.render('managementPOView', {rows});
-            }else{
+            if (!err) {
+                res.render('managementPOView', { rows });
+            } else {
                 console.log(err);
             }
 
-            
+
         });
-       
+
     });
 
 }
 
 
 exports.riddirectMainPage = (req, res) => {
- 
-     res.render('webHome');
-    
+
+    res.render('webHome');
+
 }
 exports.riddirectSupplierPage = (req, res) => {
- 
+
     res.render('supplierViewPurchasedOrder');
-   
+
 }
 
 exports.viewRequisition = (req, res) => {
-    
+
 
     //connect to DB
     pool.getConnection((err, connection) => {
         if (err) throw err; // not connected
         console.log('Connected as ID' + connection.threadId);
 
-        connection.query('SELECT * FROM solidbuilders.purchaseorder where approval =? or approval =? or approval =?' ,["Needed", "Approve", "Management"], (err,rows) =>{
-            
+        connection.query('SELECT * FROM solidbuilders.purchaseorder where approval =? or approval =? or approval =?', ["Needed", "Approve", "Management"], (err, rows) => {
+
             connection.release();
 
-            if(!err){ 
-                res.render('requisitionManagementOrder', {rows});
-            }else{
+            if (!err) {
+                res.render('requisitionManagementOrder', { rows });
+            } else {
                 console.log(err);
             }
 
-            
+
         });
-       
+
     });
 
 }
 
 //retireving to staff, management login select
 exports.loginCompanySide = (req, res) => {
-    
+
 
     res.render('companyLoginSelect');
 
@@ -110,32 +113,32 @@ exports.loginCompanySide = (req, res) => {
 
 //view Not completed deliveries
 exports.viewNotCompletedDeliveries = (req, res) => {
-    
+
 
     //connect to DB
     pool.getConnection((err, connection) => {
         if (err) throw err; // not connected
         console.log('Connected as ID' + connection.threadId);
 
-        connection.query('SELECT * FROM solidbuilders.deliverydetails where deliveryStatus = ?' ,["Not Completed"], (err,rows) =>{
-            
+        connection.query('SELECT * FROM solidbuilders.deliverydetails where deliveryStatus = ?', ["Not Completed"], (err, rows) => {
+
             connection.release();
 
-            if(!err){ 
-                res.render('staffViewNCompletedDeliveries', {rows});
-            }else{
+            if (!err) {
+                res.render('staffViewNCompletedDeliveries', { rows });
+            } else {
                 console.log(err);
             }
 
-            
+
         });
-       
+
     });
 
 }
 
 //go to payment form
-exports.goToAddPaymentPage = (req,res) => {
+exports.goToAddPaymentPage = (req, res) => {
 
     res.render('staffAddPayment');
 
@@ -157,7 +160,7 @@ exports.managementLoginView = (req, res) => {
 
 //retireving to supplier login select
 exports.supplierLogin = (req, res) => {
-    
+
 
     res.render('supplierLogin');
 
